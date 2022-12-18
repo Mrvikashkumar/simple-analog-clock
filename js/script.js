8de5996b97b5a9f8fcfaca42e26c742018171dee
Vikash Kumar <mrvikashkumar396@gmail.com>
2022-12-18 13:03:22 +0530

    add dots between two nums

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 let clockNums = document.querySelector('.clock-nums');
 let clockHandle = document.querySelector('.clock-handle');
-function printNums() {
-  for (let i = 1; i <= 12; i++) {
+function printNums(nums) {
+  for (let i = 1; i <= nums; i++) {
     let num = document.createElement('div');
-    num.innerHTML = `<div style="transform: rotate(${-i * 30}deg)">${i}</div>`;
-    num.classList.add('clock-num')
-    num.classList.add(`clock-num-${i}`)
+    tmp = i / 5
+      if(tmp!==12 && tmp!==1 && tmp!==2 && tmp!==3 && tmp!==4 && tmp!==5 && tmp!==6 && tmp!==7 && tmp!==8 && tmp!==9 && tmp!==10 && tmp!==11){
+        tmp = "."
+      }
+    num.innerHTML = `<div style="transform: rotate(${-i * 6}deg)">${tmp}</div>`;
+    num.classList.add(`clock-num`, `clock-num-${i}`)
     clockNums.append(num)
-    if (num.textContent == i) {
-      let numRotate = document.querySelector(`.clock-num-${i}`)
-      numRotate.style.transform = `rotate(${i * 30}deg)`
-    }
+    let numRotate = document.querySelector(`.clock-num-${i}`)
+    numRotate.style.transform = `rotate(${i * 6}deg)`;
   }
 }
-printNums();
+printNums(60);
